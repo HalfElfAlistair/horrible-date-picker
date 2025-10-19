1ab50461f78d31c67303c2f536668122a91eb9e0
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2025-10-19 19:52:11 +0100

    changing how messages are determined for final stage, as well as adding a new message

--- src/components/stages/StageFive.jsx
+++ src/components/stages/StageFive.jsx
@@ -2,28 +2,26 @@ export const StageFive = ({ birthDate }) => {
 
     const { year, month, day } = birthDate;
 
-    const date = new Date(birthDate.year, birthDate.month - 1, birthDate.day);
+    const date = new Date(year, month - 1, day);
     const timestamp = date.getTime();
     const currentTimeStamp = Date.now();
 
-    console.log('date', date)
-
-    const checkAge = (type) => {
-        return (currentTimeStamp - type) > timestamp;
+    const checkAge = () => {
+        if ((currentTimeStamp - 3153600000000) > timestamp) {
+            return 'Wait are you an elf? Congratulations on clearing 100, you definitely pass this one, you big oldie!'
+        } else if ((currentTimeStamp - 567648000000) > timestamp) {
+            return "Nice one, you're over 18 and pass the verification test."
+        } else if (timestamp > currentTimeStamp) {
+            return "Ah a time traveller, how interesting. Please do get in touch and let me know the lottery numbers for next week."
+        } else {
+            return "Ah sorry, you're not old enough for this page."
+        }
     }
 
     return (
         <div className='stageContainer flex-center'>
             <div className='textContainer'>
-                {checkAge(3153600000000) ? (
-                    <p>Wait are you an elf? Congratulations on clearing 100, you definitely pass this one, you big oldie!</p>
-                ) : (
-                    checkAge(567648000000) ? (
-                        <p>Nice one, you're over 18 and pass the verification test.</p>
-                    ) : (
-                        <p>Ah sorry, you're not old enough for this page.</p>
-                    )
-                )}
+                <p>{checkAge()}</p>
             </div>
         </div>
     )
