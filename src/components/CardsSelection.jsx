951fb769b40740d4cd85c19158e80927b18947d5
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2025-10-21 22:48:14 +0100

    replacing static default cards object with shorter, generated one. Also adding hover titles to cards

--- src/components/CardsSelection.jsx
+++ src/components/CardsSelection.jsx
@@ -2,268 +2,26 @@ import { Card } from "./svgs/Card";
 import { Tick } from "./svgs/Tick";
 import { useState } from 'react';
 export const CardsSelection = ({ updateBirthDate }) => {
-    const defaultCards = {
-        sa: {
-            text: 'A',
-            value: 1,
-            symbol: 'spade'
-        },
-        s2: {
-            text: '2',
-            value: 2,
-            symbol: 'spade'
-        },
-        s3: {
-            text: '3',
-            value: 3,
-            symbol: 'spade'
-        },
-        s4: {
-            text: '4',
-            value: 4,
-            symbol: 'spade'
-        },
-        s5: {
-            text: '5',
-            value: 5,
-            symbol: 'spade'
-        },
-        s6: {
-            text: '6',
-            value: 6,
-            symbol: 'spade'
-        },
-        s7: {
-            text: '7',
-            value: 7,
-            symbol: 'spade'
-        },
-        s8: {
-            text: '8',
-            value: 8,
-            symbol: 'spade'
-        },
-        s9: {
-            text: '9',
-            value: 9,
-            symbol: 'spade'
-        },
-        s10: {
-            text: '10',
-            value: 0,
-            symbol: 'spade'
-        },
-        sj: {
-            text: 'J',
-            value: 0,
-            symbol: 'spade'
-        },
-        sq: {
-            text: 'Q',
-            value: 0,
-            symbol: 'spade'
-        },
-        sk: {
-            text: 'K',
-            value: 0,
-            symbol: 'spade'
-        },
-        ha: {
-            text: 'A',
-            value: 1,
-            symbol: 'heart'
-        },
-        h2: {
-            text: '2',
-            value: 2,
-            symbol: 'heart'
-        },
-        h3: {
-            text: '3',
-            value: 3,
-            symbol: 'heart'
-        },
-        h4: {
-            text: '4',
-            value: 4,
-            symbol: 'heart'
-        },
-        h5: {
-            text: '5',
-            value: 5,
-            symbol: 'heart'
-        },
-        h6: {
-            text: '6',
-            value: 6,
-            symbol: 'heart'
-        },
-        h7: {
-            text: '7',
-            value: 7,
-            symbol: 'heart'
-        },
-        h8: {
-            text: '8',
-            value: 8,
-            symbol: 'heart'
-        },
-        h9: {
-            text: '9',
-            value: 9,
-            symbol: 'heart'
-        },
-        h10: {
-            text: '10',
-            value: 0,
-            symbol: 'heart'
-        },
-        hj: {
-            text: 'J',
-            value: 0,
-            symbol: 'heart'
-        },
-        hq: {
-            text: 'Q',
-            value: 0,
-            symbol: 'heart'
-        },
-        hk: {
-            text: 'K',
-            value: 0,
-            symbol: 'heart'
-        },
-        ca: {
-            text: 'A',
-            value: 1,
-            symbol: 'club'
-        },
-        c2: {
-            text: '2',
-            value: 2,
-            symbol: 'club'
-        },
-        c3: {
-            text: '3',
-            value: 3,
-            symbol: 'club'
-        },
-        c4: {
-            text: '4',
-            value: 4,
-            symbol: 'club'
-        },
-        c5: {
-            text: '5',
-            value: 5,
-            symbol: 'club'
-        },
-        c6: {
-            text: '6',
-            value: 6,
-            symbol: 'club'
-        },
-        c7: {
-            text: '7',
-            value: 7,
-            symbol: 'club'
-        },
-        c8: {
-            text: '8',
-            value: 8,
-            symbol: 'club'
-        },
-        c9: {
-            text: '9',
-            value: 9,
-            symbol: 'club'
-        },
-        c10: {
-            text: '10',
-            value: 0,
-            symbol: 'club'
-        },
-        cj: {
-            text: 'J',
-            value: 0,
-            symbol: 'club'
-        },
-        cq: {
-            text: 'Q',
-            value: 0,
-            symbol: 'club'
-        },
-        ck: {
-            text: 'K',
-            value: 0,
-            symbol: 'club'
-        },
-        da: {
-            text: 'A',
-            value: 1,
-            symbol: 'diamond'
-        },
-        d2: {
-            text: '2',
-            value: 2,
-            symbol: 'diamond'
-        },
-        d3: {
-            text: '3',
-            value: 3,
-            symbol: 'diamond'
-        },
-        d4: {
-            text: '4',
-            value: 4,
-            symbol: 'diamond'
-        },
-        d5: {
-            text: '5',
-            value: 5,
-            symbol: 'diamond'
-        },
-        d6: {
-            text: '6',
-            value: 6,
-            symbol: 'diamond'
-        },
-        d7: {
-            text: '7',
-            value: 7,
-            symbol: 'diamond'
-        },
-        d8: {
-            text: '8',
-            value: 8,
-            symbol: 'diamond'
-        },
-        d9: {
-            text: '9',
-            value: 9,
-            symbol: 'diamond'
-        },
-        d10: {
-            text: '10',
-            value: 0,
-            symbol: 'diamond'
-        },
-        dj: {
-            text: 'J',
-            value: 0,
-            symbol: 'diamond'
-        },
-        dq: {
-            text: 'Q',
-            value: 0,
-            symbol: 'diamond'
-        },
-        dk: {
-            text: 'K',
-            value: 0,
-            symbol: 'diamond'
-        },
-    };
+    let defaultCards = {};
+    ['spades', 'hearts', 'clubs', 'diamonds'].forEach(suit => {
+        ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'].forEach(card => {
+            const cardLetterNumber = card[0];
+            const cardID = `${suit[0]}${card === '10' ? '10' : cardLetterNumber.toLowerCase()}`
+            const assignValue = () => {
+                if (card.length > 1) {
+                    return card === 'Ace' ? 1 : 0;
+                } else {
+                    return +card;
+                }
+            }
+            defaultCards[cardID] = {
+                name: `${card} of ${suit}`,
+                text: card === '10' ? '10' : cardLetterNumber,
+                value: assignValue(),
+                symbol: suit
+            }
+        })
+    })
     const defaultCardIDs = Object.keys(defaultCards);
     const [currentCards, setCurrentCards] = useState({});
     const drawCards = () => {
@@ -324,13 +82,14 @@ export const CardsSelection = ({ updateBirthDate }) => {
             </div>
             <div className='cardsHand'>
                 {sortedCurrentCardIDs.map((cardID, i) => {
-                    const { text, symbol } = currentCards[cardID];
+                    const { text, symbol, name } = currentCards[cardID];
                     const lockedStatus = currentCards[cardID].locked;
                     return (
                         <div
                             key={cardID}
                             className='cardContainer'
                             onClick={() => lockedStatus ? unLockFunction(cardID) : lockFunction(cardID, i)}
+                            title={`${name} - ${lockedStatus ? 'unlock' : 'lock'}`}
                         >
                             <Card
                                 cardID={cardID}
